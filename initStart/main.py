2a5d2631521a16f30fa7b0073b3b5bfa1dddec7e
David Olson <deolson@pugetsound.edu>
2017-03-14 21:06:20 -0700

    finished getting started

--- initStart/main.py
+++ initStart/main.py
@@ -8,3 +8,7 @@ if __name__ == '__main__':
     x = np.array([1,2,3,4])
     y = np.array([0,-1,-2,-3])
     input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x}, y, batch_size=4, num_epochs=1000)
+
+    estimator.fit(input_fn=input_fn, steps=1000)
+
+    print(estimator.evaluate(input_fn=input_fn))
